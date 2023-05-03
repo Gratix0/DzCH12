ff681a9c8dc7b6d826d3bc89a5b13814f3281cca
Gratix0 <oganisyan.sanya1@mail.ru>
2023-05-03 19:07:21 +0300

    1234123

--- testProject/newproject/testtestProject/admin.py
+++ testProject/newproject/testtestProject/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Person
+from .models import Mechanism
+from .models import AllRange
+from .models import Preferences
+from .models import Spot
 
 # Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display=('name','lastname')
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(AllRange)
+admin.site.register(Preferences)
+admin.site.register(Spot)
+admin.site.register(Mechanism)
+
--- testProject/newproject/testtestProject/models.py
+++ testProject/newproject/testtestProject/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+
+
+class Mechanism(models.Model):
+    switch = models.CharField(max_length=200)
+    keycup = models.CharField(max_length=200)
+
+class Preferences(models.Model):
+    switchP = models.CharField(max_length=200)
+    keycupP = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+
+class AllRange(models.Model):
+    keyboard = models.BigIntegerField()
+    switch = models.BigIntegerField()
+    keycup = models.BigIntegerField()
+    
+class Spot(models.Model):
+    adress = models.CharField(max_length=300)
+    localRangeAllProduct = models.BigIntegerField()
+    registry = models.DateField()
